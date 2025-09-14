d752302e8b5181a883237b02c709fe13b7e895eb
Divine Benjamin <divinebenjamin2004@gmail.com>
2025-09-15 00:58:56 +0100

    recover | completed

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     printf("Usage: ./recover cardfile\n");
     return 1;
   }
-
+ 
   // 
   BYTE buffer[BLOCK_SIZE];
 
@@ -27,9 +27,44 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  // image file tracking
+  int count = 0;
+  char filename[8];
+  FILE *img = NULL;
+
+
   // read 512bytes of memory from card
   while(fread(buffer, 1, BLOCK_SIZE, card) == BLOCK_SIZE)
   {
+    // check if it's a jpeg
+    if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+    {
+      // close previous jpg file
+      if(img != NULL)
+      {
+        fclose(img);
+      }
+
+      // open and write new jpeg file
+      count++;
+      sprintf(filename, "%03i.jpg", count);
+      img = fopen(filename, "w");
+    }
+    
+    // keep writing to opened file
+    if(img != NULL)
+    {
+      fwrite(buffer, 1, BLOCK_SIZE, img);
+    }
 
   }
+
+  // close files
+  fclose(card);
+  if (img != NULL)
+  {
+    fclose(img);
+  }
+  
+  return 0;
 }
